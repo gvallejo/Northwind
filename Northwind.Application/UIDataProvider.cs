f223d81045a75bfba0a7405e199b9b8bebb6fd4a
Gerardo Vallejo <gvallejor@gmail.com>
2016-07-31 18:19:29 -0600

    Cosmetic changes to code files

--- Northwind.Application/UIDataProvider.cs
+++ Northwind.Application/UIDataProvider.cs
@@ -7,10 +7,12 @@ namespace Northwind.Application
     public class UIDataProvider : IUIDataProvider
     {
         private readonly CustomerServiceClient _customerServiceClient = new CustomerServiceClient();
+
         public IList<Model.Customer> GetCustomers()
         {
             return _customerServiceClient.GetCustomers().Select(c => new Model.Customer().Update(c)).ToList();
         }
+
         public Model.Customer GetCustomer(string customerID)
         {
             return new Model.Customer().Update(_customerServiceClient.GetCustomer(customerID));
