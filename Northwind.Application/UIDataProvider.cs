6a62096a26c1f688bfddbd91bfee7fa32ead8fc8
Gerardo Vallejo <gvallejor@gmail.com>
2016-08-01 13:02:43 -0600

    Inline spaces fixed. Commited from visual studio git client

--- Northwind.Application/UIDataProvider.cs
+++ Northwind.Application/UIDataProvider.cs
@@ -8,14 +8,17 @@ namespace Northwind.Application
     {
         private IList<Model.Customer> _customers;
         private readonly ICustomerService _customerServiceClient;
+
         public UIDataProvider(ICustomerService customerService)
         {
             _customerServiceClient = customerService;
         }
+
         public IList<Model.Customer> GetCustomers()
         {
             return _customers ?? (_customers = _customerServiceClient.GetCustomers().Select(c => CustomerTranslator.Instance.CreateModel(c)).ToList());
         }
+
         public Model.Customer GetCustomer(string customerID)
         {
             return CustomerTranslator.Instance.UpdateModel(_customers.First(c => c.CustomerID == customerID), _customerServiceClient.GetCustomer(customerID));
